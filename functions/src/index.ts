5742ff08feaf8e3ee009c704554bc41c8360c4ed
Hoob Innovation <138602355+hoobinnovation@users.noreply.github.com>
2026-03-02 14:06:55 +0200

    Rename public gateway binding to avoid reserved identifier

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import { setGlobalOptions } from 'firebase-functions/v2';
-import { public } from './gateways/publicGateway';
+import { publicGateway } from './gateways/publicGateway';
 import { client } from './gateways/clientGateway';
 import { admin } from './gateways/adminGateway';
 import { storageThumbnails } from './triggers/storageThumbnails';
@@ -7,7 +7,7 @@ import { storageThumbnails } from './triggers/storageThumbnails';
 setGlobalOptions({ region: 'us-central1', memory: '256MiB', timeoutSeconds: 60, maxInstances: 20 });
 
 export {
-  public,
+  publicGateway as public,
   client,
   admin,
   storageThumbnails,
