2d3689b0324a6faea5ca37204eee05dd887656aa
Ziyad Mohammed <hoobinnovation@gmail.com>
2026-03-12 00:27:22 +0200

    1

--- functions/src/dispatch/dispatchAction.ts
+++ functions/src/dispatch/dispatchAction.ts
@@ -275,6 +275,7 @@ export async function dispatchAction(
             meta: { requestId: ctx.requestId, serverTime: ctx.serverTime },
         };
     } catch (err) {
+        console.error(err
         return toErrorResponse(err, ctx.requestId, ctx.serverTime);
     }
 }
